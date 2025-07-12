462aadcaf781ae676912a745b38d50bdb2277545
rushilx <rushilverma7@gmail.com>
2025-07-12 20:55:29 +0530

    g

--- app/components/helper/GTMClient.jsx
+++ app/components/helper/GTMClient.jsx
@@ -1,8 +1,35 @@
 "use client";
 
-import dynamic from "next/dynamic";
-const DynamicGTM = dynamic(() => import("@next/third-parties/google").then(mod => mod.GoogleTagManager), { ssr: false });
+import { useEffect } from "react";
 
 export default function GTMClient() {
-  return <DynamicGTM gtmId={process.env.NEXT_PUBLIC_GTM} />;
+  useEffect(() => {
+    // GTM script
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GTM}`;
+    document.head.appendChild(script);
+
+    // GTM inline script
+    const inlineScript = document.createElement("script");
+    inlineScript.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM}');`;
+    document.head.appendChild(inlineScript);
+
+    // Noscript iframe
+    const noscript = document.createElement("noscript");
+    const iframe = document.createElement("iframe");
+    iframe.src = `https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM}`;
+    iframe.height = "0";
+    iframe.width = "0";
+    iframe.style.display = "none";
+    iframe.style.visibility = "hidden";
+    noscript.appendChild(iframe);
+    document.body.insertBefore(noscript, document.body.firstChild);
+  }, []);
+
+  return null;
 } 
