8cd2ed93288056fd94cff0407e6762610cc2054e
rushilx <rushilverma7@gmail.com>
2025-07-12 21:02:28 +0530

    das

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import { personalData } from "@/utils/data/personal-data";
-import AboutSection from "./components/homepage/about";
+import dynamic from "next/dynamic"
 import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
@@ -7,6 +7,8 @@ import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+
+const AboutSection = dynamic(() => import("./components/homepage/about"), { ssr: false })
 
 async function getData() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
